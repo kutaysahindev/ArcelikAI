ca3db213b4593b1b7d3c47310ff11a8dc5778556
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2023-12-08 17:40:12 +0300

    Style changes has been applied to Navbar Component.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = ({ onItemClick }) => {
       <img className='' src={arclk} alt='arcelik' />
     </div>
     <div className="steps">
-      <div onClick={() => onItemClick(0)} className='step'>Home</div>
+      <div onClick={() => onItemClick(0)} className='step active'>Home</div>
       <div onClick={() => onItemClick(1)} className='step'>Getting Started</div>
       <div onClick={() => onItemClick(2)} className='step'>Prompt Library</div>
       <div onClick={() => onItemClick(3)} className='step'>Ethics & Security</div>
