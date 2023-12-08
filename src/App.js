d81ef6228ad530480bdfff16f0fe27d1dd83bbac
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-12-08 16:39:09 +0300

    Create main component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleNavbarItemClick = (index) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      <Navbar onItemClick={handleNavbarItemClick} />
+      <Main selectedIndex={selectedIndex} />
       <Footer />
     </div>
   );
