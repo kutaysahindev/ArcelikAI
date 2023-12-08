254bb5e025d0eb8bcaeb037cf1a548da94046e57
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2023-12-08 17:20:11 +0300

    Navbar has been configured.(hsn)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Navbar.css";
+import arclk from '../../assets/arcelik_logo_uzun 1.png'
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({ onItemClick }) => {
+  return <nav id="navbar">
+    <div className="logo">
+      <img className='' src={arclk} alt='arcelik' />
+    </div>
+    <div className="steps">
+      <div onClick={() => onItemClick(0)} className='step'>Home</div>
+      <div onClick={() => onItemClick(1)} className='step'>Getting Started</div>
+      <div onClick={() => onItemClick(2)} className='step'>Prompt Library</div>
+      <div onClick={() => onItemClick(3)} className='step'>Ethics & Security</div>
+    </div>
+  </nav>;
 };
 
 export default Navbar;
