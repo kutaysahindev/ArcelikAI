12f13145b9605824fbc5a0d68dc1cd1665c1ea5c
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-12-11 16:34:52 +0300

    Revert "!! SAKIN Merge'lemeyin Patladı"
    This reverts commit 752d9b90b397ac3635065933fa8451533ac2656c.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-// Import the necessary components and modules
-import { Security, SecureRoute, LoginCallback } from "@okta/okta-react";
-import { OktaAuth } from "@okta/okta-auth-js";
-import oktaConfig from "./oktaConfig";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import { useState } from "react";
 
-// Function to configure and render the main App component
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const oktaAuth = new OktaAuth(oidcConfig);
+
   const handleNavbarItemClick = (index) => {
     setSelectedIndex(index);
   };
 
   return (
-    <Security
-      {...oktaConfig}
-      oktaAuth={oktaAuth}
-      onAuthRequired={customAuthHandler}
-    >
-      <div className="App">
-        <Navbar
-          onItemClick={handleNavbarItemClick}
-          selectedIndex={selectedIndex}
-        />
-        <Main selectedIndex={selectedIndex} />
-        <Footer />
-      </div>
-    </Security>
+    <div className="App">
+      <Navbar onItemClick={handleNavbarItemClick} selectedIndex={selectedIndex} />
+      <Main selectedIndex={selectedIndex} />
+      <Footer />
+    </div>
   );
 }
 
--- src/oktaConfig.js
+++
@@ -1,7 +0,0 @@
-const oktaConfig = {
-  clientId: "0oadr4g28pCAmDGSS5d7",
-  issuer: "https://dev-69297377.okta.com",
-  redirectUri: window.location.origin + "/login/callback",
-};
-
-export default oktaConfig;
