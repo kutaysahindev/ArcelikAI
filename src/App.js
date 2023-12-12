6d9ab695a83435807f898b5c9b039e355806ceec
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-12-12 16:00:17 +0300

    Okta data fetch & Logout state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
-import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { OktaAuth } from "@okta/okta-auth-js";
 import { Security } from "@okta/okta-react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginCallback } from "@okta/okta-react"; // Add this line
+
+import Profile from "./Profile";
 
 const oktaAuth = new OktaAuth({
   issuer: "https://dev-16420108.okta.com/oauth2/default",
   clientId: "0oadroi27bvehMs8M5d7",
   redirectUri: window.location.origin + "/login/callback",
 });
+
+const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+  window.location.assign(originalUri);
+};
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -19,23 +27,23 @@ function App() {
     setSelectedIndex(index);
   };
 
-  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-    window.location.replace(
-      toRelativeUrl(originalUri || "/", window.location.origin)
-    );
-  };
-
   return (
-    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
-      <div className="App">
-        <Navbar
-          onItemClick={handleNavbarItemClick}
-          selectedIndex={selectedIndex}
-        />
-        <Main selectedIndex={selectedIndex} />
-        <Footer />
-      </div>
-    </Security>
+    <Router>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+        <div className="App">
+          <Navbar
+            onItemClick={handleNavbarItemClick}
+            selectedIndex={selectedIndex}
+          />
+          <Main selectedIndex={selectedIndex} />
+          <Footer />
+          <Routes>
+            <Route path="/login/callback" element={<LoginCallback />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </Security>
+    </Router>
   );
 }
 
