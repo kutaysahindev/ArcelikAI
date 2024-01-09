ea1bfcab3505b9704fc9d7b5de01acfa9171c6d1
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-01-09 14:16:19 +0300

    Video status updates & Major improvements

---
+++ ArcelikWebApi/ArcelikWebApi/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArcelikWebApi.Models
+{
+    public class Users
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        public string Email { get; set; }
+
+        public bool isWatched { get; set; }
+    }
+}
