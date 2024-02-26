d5200f3944213f3138151575e1a56996158faf4c
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2024-02-26 13:30:33 +0300

    fetching questions now

--- src/components/QuestionDashboard/QuestionPool.jsx
+++ src/components/QuestionDashboard/QuestionPool.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getQuestions } from "../../api";
 import { useSelector } from "react-redux";
-import { questions } from '../../utils/questions.js'
+// import { questions } from '../../utils/questions.js'
 import { PiSelectionAllDuotone, PiSelectionAllFill } from "react-icons/pi";
 import { FaRegTrashAlt } from 'react-icons/fa';
 
@@ -9,12 +9,13 @@ import './QuestionPool.css'
 
 export const QuestionPool = () => {
   const [fixedQuestions, setFixedQuestions] = useState([]);
-  const [questionsDB, setQuestionsDB] = useState(questions);
+  const [questionsDB, setQuestionsDB] = useState([]);
   const {accessToken}  = useSelector(state => state.user)
-  const isFixed = (id) => fixedQuestions.find(q => q.Id === id)
-  // const toggleFixed = (q) => isFixed(q.Id) ? setFixedQuestions(fixedQuestions.filter(que => que.Id !== q.Id)) : setFixedQuestions(prev => [...prev, q])
-  const toggleFixed = (q) => setFixedQuestions(prev => isFixed(q.Id) ? prev.filter(que => que.Id !== q.Id) : [...prev, q])
-
+  
+  const isFixed = (id) => fixedQuestions.find(q => q.QuestionID === id)
+  const toggleFixed = (q) => setFixedQuestions(prev => isFixed(q.QuestionID) ? prev.filter(que => que.QuestionID !== q.QuestionID) : [...prev, q])
+  const deleteItem = (q) => console.log('delete: ', q)
+  
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
@@ -36,17 +37,18 @@ export const QuestionPool = () => {
       <p className="info">To make the questions fixed, click to <PiSelectionAllDuotone/></p>
       <div className='que-container'>
         {questionsDB.map(q => (
-          <div className="que-item" key={q.Id}>
-            <p className="que">{ q.question }</p>
+          <div className="que-item" key={q.QuestionID}>
+            <p className="que">{ q.QuestionText }</p>
             <div className='buttons'>
+            <p className="type">{ q.QuestionType }</p>
               <div onClick={() => toggleFixed(q)}>
-              {isFixed(q.Id) ? (
-                <PiSelectionAllFill className="qb" size={20}/>
-                ) : (
-                  <PiSelectionAllDuotone className="qb" size={20}/>
-                  )}
+                {isFixed(q.QuestionID) ? (
+                  <PiSelectionAllFill className="qb fill" size={20}/>
+                  ) : (
+                  <PiSelectionAllDuotone className="qb blank" size={20}/>
+                )}
               </div>
-              <FaRegTrashAlt className="qb" size={20}/>
+              <FaRegTrashAlt onClick={() => deleteItem(q)} className="qb delete" size={20}/>
             </div>
           </div>
         ))}
